da3c4e9af84654e1f75d7fcf64d386962797ef58
Alban Ngwa <Ngwaambe51@gmail.com>
2024-12-30 10:37:34 -0600

    fixed more errors

--- vpc/main.tf
+++ vpc/main.tf
@@ -24,7 +24,7 @@ data "aws_availability_zones" "available_zones" {}
 # create public subnet az1
 resource "aws_subnet" "public_subnet_az1" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public_subnet_az1-cidr
+  cidr_block              = var.public_subnet_az1_cidr
   availability_zone       = data.aws_availability_zones.available_zones[0]
   map_public_ip_on_launch = true 
 
@@ -36,7 +36,7 @@ resource "aws_subnet" "public_subnet_az1" {
 # create public subnet az2
 resource "aws_subnet" "public_subnet_az2" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public_subnet_az2-cidr
+  cidr_block              = var.public_subnet_az2_cidr
   availability_zone       = data.aws_availability_zones.available_zones[1]
   map_public_ip_on_launch = true 
 
